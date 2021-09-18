78f1478ecf99a9a1a1aa9fc4806c593884335255
swpark907 <76856321+swpark907@users.noreply.github.com>
2021-09-18 16:54:22 +0900

    input autoComplete, item selected

--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
 
 function Search({items}) {
+
+    function autoComplete(e){
+        const currentValue = e.target.value;
+        const items = document.querySelectorAll('.item');
+        items.forEach(li => {
+            
+            li.classList.remove('active');
+            if(li.dataset.name.includes(e.target.value)){
+                li.classList.add('active')
+            }
+        })
+
+    }
+
+    function selected(e){
+        if(e.currentTarget.classList.contains('selected')){
+            e.currentTarget.classList.remove('selected')
+        } else{
+        e.currentTarget.classList.add('selected')
+    }
+    }
+
     
     return (
         <div className='main'>
             <h2>냉털생활</h2>
-            <input type="text" />
+            <input type="text" onChange={(e) => autoComplete(e)} />
             <ul className="items-list">
             {
                 items.items.map((item) => 
-                <li className="item" key={item.id}> {item.name} 
+                <li className="item active" onClick={e=>selected(e)} key={item.id} data-name={item.name}> {item.name} 
                 <button className='item-cancel'>X</button>
                 </li>
                 )
