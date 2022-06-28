847235f18e121df7c687ef89749b48c7d3a504a8
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-06-28 23:20:53 +0900

    fix: 필요없는 코드 제거

--- src/service/naver_search.js
+++ src/service/naver_search.js
@@ -3,18 +3,22 @@ import axios from 'axios';
 class Naver {
     constructor(id, secret){
         this.id = id;
-        this.secret = secret;
+        this.secret = secret
         this.naver = axios.create({
-            baseURL: 'https://openapi.naver.com/v1/',
-            'Access-Control-Allow-Origin' : '*',
-            headers: {                
+            baseURL: 'https://cors-anywhere.herokuapp.com/https://openapi.naver.com/v1/',
+            headers: {
                 "X-Naver-Client-Id": this.id,
                 "X-Naver-Client-Secret": this.secret,
+                // 'Access-Control-Allow-Credentials': true,
+                // 'Access-Control-Allow-Origin' : 'http://localhost:3000/',
+                // 'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+                // 'Access-Contron-Allow-Headers':'Origin,X-Requested-With,Content-Type,Accept',
             },
         });
     }
 
     async search(query){
+        console.log(query + ' in naver');
         try{
             const response = await this.naver.get('search/blog',
             {   
@@ -23,6 +27,7 @@ class Naver {
                     display: 5,
                 }
             })
+            console.log(response.data.items);
             return response.data.items;
         } catch(error){console.log(error)};
         
